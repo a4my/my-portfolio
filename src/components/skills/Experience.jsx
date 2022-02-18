92af478f429203c95081e5d4bcd885d488d4d22c
Alex Fourmy <alexfourmy@gmail.com>
2022-02-18 19:35:01 +0100

    Completed the experience components of the skills page

---
+++ src/components/skills/Experience.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Experience() {
+  return (
+    <div className="experience">
+      <h3>Experience</h3>
+      <div className="exp1">
+        <h4>VAT Registration Representative - Sovos Accordance</h4>
+        <h5>November 2019 - April 2021</h5>
+        <p>
+          Support clients throughout the VAT registration process, collating all
+          information required and liaising with European Tax Authorities and
+          internal departments.
+        </p>
+      </div>
+      <div className="exp2">
+        <h4>Customer Relationship Manager - Fender</h4>
+        <h5>January 2018 - Ocotber 2019</h5>
+        <p>
+          Assist customers, District Sales Managers and retailers with enquiries
+          related to customer service, orders, stock, product information and
+          returns. Awarded “Employee of the Year” 2018. Strive to achieve
+          budgeted sales targets for allocated sales areas. Established and
+          maintained positive working relationships with customers and potential
+          new clients.
+        </p>
+      </div>
+    </div>
+  )
+}
