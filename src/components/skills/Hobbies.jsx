8f8789c42f607b649739754d1151b918782e615f
Alex Fourmy <alexfourmy@gmail.com>
2022-02-18 19:35:22 +0100

    Completed the hobbies component of the skills page

---
+++ src/components/skills/Hobbies.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Hobbies() {
+  return (
+    <div className="hobbies">
+      <h3>Interests</h3>
+      <ul>
+        <li>
+          <img src="./media/icons/science.svg" alt="" />
+          <span>Science</span>
+        </li>
+        <li>
+          <img src="./media/icons/rubgy.svg" alt="" />
+          <span>Rugby</span>
+        </li>
+        <li>
+          <img src="./media/icons/controller.svg" alt="" />
+          <span>Gaming</span>
+        </li>
+        <li>
+          <img src="./media/icons/rocket.svg" alt="" />
+          <span>Space</span>
+        </li>
+        <li>
+          <img src="./media/icons/bitcoin.svg" alt="" />
+          <span>Cryptocurrencies</span>
+        </li>
+      </ul>
+    </div>
+  )
+}
