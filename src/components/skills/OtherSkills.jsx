5840142e77d946470eb8c2ae911f39e1141acde8
Alex Fourmy <alexfourmy@gmail.com>
2022-02-18 19:35:58 +0100

    Completed the other skills component of the skills page

---
+++ src/components/skills/OtherSkills.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function OtherSkills() {
+  return (
+    <div className="otherSkills">
+      <h3>Other skills</h3>
+      <div className="list">
+        <ul>
+          <li>
+            <i className="fas fa-check-square"></i>Express
+          </li>
+          <li>
+            <i className="fas fa-check-square"></i>MongoDB
+          </li>
+          <li>
+            <i className="fas fa-check-square"></i>Github
+          </li>
+          <li>
+            <i className="fas fa-check-square"></i>VS Code
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <i className="fas fa-check-square"></i>French, English, Spanish,
+            Japanese
+          </li>
+          <li>
+            <i className="fas fa-check-square"></i>UI/UX Design
+          </li>
+          <li>
+            <i className="fas fa-check-square"></i>Final Cut Pro
+          </li>
+          <li>
+            <i className="fas fa-check-square"></i>Cubase SX3
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
