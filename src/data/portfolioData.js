d6bc4a362c5ebf93522dcbcf20d764f9753ac883
Alex Fourmy <alexfourmy@gmail.com>
2022-02-19 18:51:22 +0100

    Listed all projects to appear on the portfolio

---
+++ src/data/portfolioData.js
@@ -0,0 +1,132 @@
+export const portfolioData = [
+  {
+    id: 1,
+    name: 'All In One',
+    languages: ['javascript', 'css'],
+    languagesIcons: ['./media/icons/css3.svg', './media/icons/javascript.svg'],
+    source: 'https://github.com/a4my/all-in-one-page',
+    demo: 'https://a4my.github.io/all-in-one-page/',
+    info: 'This repository contains the files of my very first JavaScript project! This project is supposed to be an all-in-one pager, a page that you would keep open on one of your screen and where you can access in one click the basic information and tools you normally use when you are using your PC. This page regroups information suchs as the date and time, live cryptocurrency rates, live weather forecast, shortcuts to all my social media profiles and other websites I use frequently (StackOverflow, Google, GitHub, etc) as well as a calculator, a music player, a converter and a to-do list.',
+    picture: './media/project1.JPG'
+  },
+  {
+    id: 2,
+    name: 'Weather App',
+    languages: ['next.js', 'react', 'javascript'],
+    languagesIcons: [
+      './media/icons/javascript.svg',
+      './media/icons/nextjs.svg',
+      './media/icons/react.svg'
+    ],
+    source: 'https://github.com/a4my/react-weather-forecast',
+    demo: 'https://react-weather-forecast-a4my.vercel.app/',
+    info: 'This is a weather forecast app built with React and Next.js. Users can search for a city and get hourly, daily and weekly information about the weather coming from an API.',
+    picture: './media/project2.JPG'
+  },
+  {
+    id: 3,
+    name: 'Planet Rugby',
+    languages: ['html', 'css', 'sass'],
+    languagesIcons: [
+      './media/icons/html.svg',
+      './media/icons/css3.svg',
+      './media/icons/sass.svg'
+    ],
+    source: 'https://github.com/a4my/planet-rugby-homepage',
+    demo: 'https://a4my.github.io/planet-rugby-homepage/',
+    info: 'This is the very first project I have ever built alone and it is about one of my many passions: rugby! 😁 This fictional rugby webzine is also a multi-page website.',
+    picture: './media/project3.JPG'
+  },
+  {
+    id: 4,
+    name: 'Explore',
+    languages: ['html', 'css', 'sass'],
+    languagesIcons: [
+      './media/icons/html.svg',
+      './media/icons/css3.svg',
+      './media/icons/sass.svg'
+    ],
+    source: 'https://github.com/a4my/spaceship-dealer-website',
+    demo: 'https://a4my.github.io/spaceship-dealer-website/',
+    info: 'This project is supposed to be a fictional website for a space ship dealer set in the year 2122. This project has parts that are mainly hard-coded and you cannot add item in your cart and proceed with payment. This is more of a HTML / CSS type of project.',
+    picture: './media/project4.JPG'
+  },
+  {
+    id: 5,
+    name: 'Movie List',
+    languages: ['javascript', 'react'],
+    languagesIcons: ['./media/icons/javascript.svg', './media/icons/react.svg'],
+    source: 'https://github.com/a4my/react-movielist',
+    demo: 'https://a4my.github.io/react-movielist/',
+    info: 'This project is one of my first React projects, it is a simple watchlist where you can add and track the movies you would like to watch or that you have already watched.',
+    picture: './media/project5.JPG'
+  },
+  {
+    id: 6,
+    name: 'Petflix',
+    languages: ['html', 'css', 'sass'],
+    languagesIcons: [
+      './media/icons/html.svg',
+      './media/icons/css3.svg',
+      './media/icons/sass.svg'
+    ],
+    source: 'https://github.com/a4my/petflix',
+    demo: 'https://a4my.github.io/petflix/',
+    info: 'This repository contains the files of one of my silly projects: Petflix! 😁 This project is supposed to be a Netflix homepage clone. This project has parts that are mainly hard-coded and you cannot add watch any movies or create an account. This is more of a HTML / CSS type of project.',
+    picture: './media/project6.JPG'
+  },
+  {
+    id: 7,
+    name: 'To Do List',
+    languages: ['javascript', 'react'],
+    languagesIcons: ['./media/icons/javascript.svg', './media/icons/react.svg'],
+    source: 'https://github.com/a4my/mern-todo-list',
+    demo: 'https://my-todolist-mern.herokuapp.com/dashboard',
+    info: 'This repository contains the files of one of my MERN projects. This is a MERN todo list where users can register and use their own todo lists. Users can register and login to access their dashboard as well as creating and tracking todo items which can also be edited, deleted or toggled between completed and incompleted status.',
+    picture: './media/project7.JPG'
+  },
+  {
+    id: 8,
+    name: 'Game List',
+    languages: ['css', 'sass', 'react'],
+    languagesIcons: [
+      './media/icons/css3.svg',
+      './media/icons/sass.svg',
+      './media/icons/react.svg'
+    ],
+    source: 'https://github.com/a4my/react-videogame',
+    demo: 'https://a4my.github.io/react-videogame/',
+    info: 'This React App is supposed to be an app that lists video games and where you can search for a video game. This project has parts that are mainly hard-coded and you cannot use the searchbar to search for a specific video game. However, the list of video games is coming from an API.',
+    picture: './media/project8.JPG'
+  },
+  {
+    id: 9,
+    name: 'Fun with Flags',
+    languages: ['javascript', 'react'],
+    languagesIcons: ['./media/icons/javascript.svg', './media/icons/react.svg'],
+    source: 'https://github.com/a4my/react-flag-search',
+    demo: 'https://flag-search.herokuapp.com/',
+    info: 'This app displays a list of all countries coming from an API and it is rendered using React. You can also sort the list by continents or number of countries to display using range and radio inputs. ',
+    picture: './media/project9.JPG'
+  },
+  {
+    id: 10,
+    name: 'Pig Game',
+    languages: ['javascript'],
+    languagesIcons: ['./media/icons/css3.svg', './media/icons/javascript.svg'],
+    source: 'https://github.com/a4my/my-pig-game',
+    demo: 'https://a4my.github.io/my-pig-game/',
+    info: 'This project is my own version of the famous Pig game and it is one the very first JavaScript project I did alone. I actually take pride in implementing a leaderboard and a modal to set the rules of the game and the names of the two players! ',
+    picture: './media/project10.JPG'
+  },
+  {
+    id: 11,
+    name: 'Blue Planet',
+    languages: ['css', 'sass'],
+    languagesIcons: ['./media/icons/css3.svg', './media/icons/javascript.svg'],
+    source: 'https://github.com/a4my/blue-planet',
+    demo: 'https://a4my.github.io/blue-planet/',
+    info: 'This project is a landing page with a caroussel as a background. At the time of building this page, the main focus was to practice making landing pages as well as practicing fundamentals html and CSS. ',
+    picture: './media/project11.JPG'
+  }
+]
