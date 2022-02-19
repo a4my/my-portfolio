a35644f301de8d77b819b21402bfc95a71a32063
Alex Fourmy <alexfourmy@gmail.com>
2022-02-19 18:52:01 +0100

    First commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 export default function Contact() {
-  return <div>Contact</div>
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
 }
