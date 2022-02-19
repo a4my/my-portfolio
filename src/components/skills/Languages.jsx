335c9492eb385a60c0feefd9e8d990172bc0e59b
Alex Fourmy <alexfourmy@gmail.com>
2022-02-19 18:49:03 +0100

    Added a language component to use on the skills page

--- src/components/skills/Languages.jsx
+++ src/components/skills/Languages.jsx
@@ -4,16 +4,56 @@ import ProgressBar from './ProgressBar'
 class Languages extends Component {
   state = {
     languages: [
-      { id: 1, value: 'HTML', xp: 1.6 },
-      { id: 2, value: 'CSS', xp: 1.6 },
-      { id: 3, value: 'JavaScript', xp: 1.4 },
-      { id: 4, value: 'NodeJS', xp: 1.1 }
+      {
+        id: 1,
+        icon: './media/icons/html.svg',
+        value: 'HTML',
+        xp: 1.6
+      },
+      {
+        id: 2,
+        icon: './media/icons/css3.svg',
+        value: 'CSS',
+        xp: 1.6
+      },
+      {
+        id: 3,
+        icon: './media/icons/javascript.svg',
+        value: 'JavaScript',
+        xp: 1.4
+      },
+      {
+        id: 4,
+        icon: './media/icons/nodejs.svg',
+        value: 'NodeJS',
+        xp: 1.1
+      }
     ],
     frameworks: [
-      { id: 1, value: 'React', xp: 1.1 },
-      { id: 2, value: 'Sass', xp: 1.4 },
-      { id: 3, value: 'Bootstrap', xp: 0.7 },
-      { id: 4, value: 'NextJS', xp: 0.4 }
+      {
+        id: 1,
+        icon: './media/icons/react.svg',
+        value: 'React',
+        xp: 1.1
+      },
+      {
+        id: 2,
+        icon: './media/icons/sass.svg',
+        value: 'Sass',
+        xp: 1.4
+      },
+      {
+        id: 3,
+        icon: './media/icons/bootstrap.svg',
+        value: 'Bootstrap',
+        xp: 0.7
+      },
+      {
+        id: 4,
+        icon: './media/icons/nextjs.svg',
+        value: 'NextJS',
+        xp: 0.4
+      }
     ]
   }
 
