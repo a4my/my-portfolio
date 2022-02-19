ce66ba2ae777f1168d9f27cc18dc9c249351538f
Alex Fourmy <alexfourmy@gmail.com>
2022-02-19 18:52:22 +0100

    Completed the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div className="home">
+      <NavBar />
+      <div className="homeContent">
+        <div className="content">
+          <h1>Alex Fourmy</h1>
+          <h2>Frontend Developer</h2>
+          <div className="pdf">
+            <a href="./media/CV.pdf" target="_blank">
+              Download CV
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
