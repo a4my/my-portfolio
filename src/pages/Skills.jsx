2b84186fef324f5c0138799e5cba038c0e002cdc
Alex Fourmy <alexfourmy@gmail.com>
2022-02-19 18:53:03 +0100

    Added final components of the skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import Experience from '../components/skills/Experience'
+import Hobbies from '../components/skills/Hobbies'
+import Languages from '../components/skills/Languages'
+import OtherSkills from '../components/skills/OtherSkills'
 
 export default function Skills() {
-  return <div>Skills</div>
+  return (
+    <div className="skills">
+      <NavBar />
+      <div className="skillsContent">
+        <Languages />
+        <Experience />
+        <OtherSkills />
+        <Hobbies />
+      </div>
+    </div>
+  )
 }
