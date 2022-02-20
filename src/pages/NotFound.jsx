cbff917e529fe5bfe02381e5709f641addc68c20
Alex Fourmy <alexfourmy@gmail.com>
2022-02-20 18:55:30 +0100

    Completed the design of the not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function NotFound() {
-  return <div>NotFound</div>
+  return (
+    <div className="notFound">
+      <div className="notFoundContent">
+        <img src="./media/notFound.svg" alt="404" />
+        <h3>Oops... no page available here!</h3>
+        <NavLink exact to="/">
+          <i className="fas fa-home"></i>
+          <span>Home</span>
+        </NavLink>
+      </div>
+    </div>
+  )
 }
