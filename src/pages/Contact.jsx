e630f6cdfa3f6c5aece47cefd1351a1b4209139b
Alex Fourmy <alexfourmy@gmail.com>
2022-02-20 18:57:07 +0100

    Started working on the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+// import { CopyToClipboard } from 'react-copy-to-clipboard'
+import ContactForm from '../components/contact/contactForm'
 
 export default function Contact() {
   return (
-    <div>
+    <div className="contact">
       <NavBar />
+      <div className="contactContainer">
+        <ContactForm />
+      </div>
     </div>
   )
 }
