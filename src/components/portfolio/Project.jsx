1c2f575fff2730a0eaa500a3249ffdb327d41423
Alex Fourmy <alexfourmy@gmail.com>
2022-02-22 19:45:15 +0100

    Added a loader img

--- src/components/portfolio/Project.jsx
+++ src/components/portfolio/Project.jsx
@@ -1,65 +1,138 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export default class Project extends Component {
-  state = {
-    showInfo: false
+export default function Project(props) {
+  const [showInfo, setShowInfo] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  let { name, languagesIcons, demo, source, info, picture } = props.item
+
+  const handleInfo = () => {
+    setShowInfo(!showInfo)
   }
 
-  handleInfo = () => {
-    this.setState({
-      showInfo: !this.state.showInfo
-    })
+  const showLoader = () => {
+    return <i className="fas fa-spinner fa-spin"></i>
   }
 
-  render() {
-    let { name, languagesIcons, demo, source, info, picture } = this.props.item
+  return (
+    <div className="project">
+      <div className="icons">
+        {languagesIcons.map(icon => (
+          <img src={icon} alt="language" />
+        ))}
+      </div>
+      <h3>{name}</h3>
 
-    return (
-      <div className="project">
-        <div className="icons">
-          {languagesIcons.map(icon => (
-            <img src={icon} alt="language" />
-          ))}
-        </div>
-        <h3>{name}</h3>
-        <img src={picture} alt="project" onClick={this.handleInfo} />
-        <span className="infos">
-          <i className="fas fa-plus-circle" onClick={this.handleInfo}></i>
-        </span>
+      <div style={{ display: isLoading ? 'block' : 'none' }}>
+        {showLoader()}
+      </div>
+      <div style={{ display: isLoading ? 'none' : 'block' }}>
+        <img
+          src={picture}
+          alt="project"
+          onClick={handleInfo}
+          onLoad={() => setIsLoading(false)}
+        />
+      </div>
+      <span className="infos">
+        <i className="fas fa-plus-circle" onClick={handleInfo}></i>
+      </span>
 
-        {this.state.showInfo && (
-          <div className="showInfos">
-            <div className="infosContent">
-              <div className="head">
-                <h2>{name}</h2>
-                <span className="cross" onClick={this.handleInfo}>
-                  &#10005;
-                </span>
-              </div>
+      {showInfo && (
+        <div className="showInfos">
+          <div className="infosContent">
+            <div className="head">
+              <h2>{name}</h2>
+              <span className="cross" onClick={handleInfo}>
+                &#10005;
+              </span>
+            </div>
 
-              <p className="text">{info}</p>
-              <div className="sourceCode">
-                <a
-                  href={source}
-                  rel="noopener noreferrer"
-                  className="button"
-                  target="_blank"
-                >
-                  Source code
-                </a>
-                <a
-                  href={demo}
-                  rel="noopener noreferrer"
-                  className="button"
-                  target="_blank"
-                >
-                  Live demo
-                </a>
-              </div>
+            <p className="text">{info}</p>
+            <div className="sourceCode">
+              <a
+                href={source}
+                rel="noopener noreferrer"
+                className="button"
+                target="_blank"
+              >
+                Source code
+              </a>
+              <a
+                href={demo}
+                rel="noopener noreferrer"
+                className="button"
+                target="_blank"
+              >
+                Live demo
+              </a>
             </div>
           </div>
-        )}
-      </div>
-    )
-  }
+        </div>
+      )}
+    </div>
+  )
 }
+
+// export default class Project extends Component {
+//   state = {
+//     showInfo: false
+//   }
+
+//   handleInfo = () => {
+//     this.setState({
+//       showInfo: !this.state.showInfo
+//     })
+//   }
+
+//   render() {
+//     let { name, languagesIcons, demo, source, info, picture } = this.props.item
+
+//     return (
+//       <div className="project">
+//         <div className="icons">
+//           {languagesIcons.map(icon => (
+//             <img src={icon} alt="language" />
+//           ))}
+//         </div>
+//         <h3>{name}</h3>
+//         <img src={picture} alt="project" onClick={this.handleInfo} />
+//         <span className="infos">
+//           <i className="fas fa-plus-circle" onClick={this.handleInfo}></i>
+//         </span>
+
+//         {this.state.showInfo && (
+//           <div className="showInfos">
+//             <div className="infosContent">
+//               <div className="head">
+//                 <h2>{name}</h2>
+//                 <span className="cross" onClick={this.handleInfo}>
+//                   &#10005;
+//                 </span>
+//               </div>
+
+//               <p className="text">{info}</p>
+//               <div className="sourceCode">
+//                 <a
+//                   href={source}
+//                   rel="noopener noreferrer"
+//                   className="button"
+//                   target="_blank"
+//                 >
+//                   Source code
+//                 </a>
+//                 <a
+//                   href={demo}
+//                   rel="noopener noreferrer"
+//                   className="button"
+//                   target="_blank"
+//                 >
+//                   Live demo
+//                 </a>
+//               </div>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     )
+//   }
+// }
