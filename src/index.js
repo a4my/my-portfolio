3add095b7ade44556b7793ff644b2d2bca3706ee
Alex Fourmy <alexfourmy@gmail.com>
2022-02-23 18:53:59 +0100

    Added a particles background underneath the whole site

--- src/index.js
+++ src/index.js
@@ -2,9 +2,91 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles/styles.scss'
+import Particles from 'react-tsparticles'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Particles
+      id="tsparticles"
+      options={{
+        background: {
+          color: {
+            value: 'transparent'
+          }
+        },
+
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: 'push'
+            },
+            onHover: {
+              enable: false,
+              mode: 'repulse'
+            },
+            resize: true
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40
+            },
+            push: {
+              quantity: 4
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4
+            }
+          }
+        },
+        particles: {
+          color: {
+            value: '#45a3da'
+          },
+          links: {
+            color: '#fff',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1
+          },
+          collisions: {
+            enable: true
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outMode: 'bounce',
+            random: false,
+            speed: 1,
+            straight: false
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800
+            },
+            value: 80
+          },
+          opacity: {
+            value: 0.5
+          },
+          shape: {
+            type: 'circle'
+          },
+          size: {
+            random: true,
+            value: 5
+          }
+        },
+        detectRetina: true
+      }}
+    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
