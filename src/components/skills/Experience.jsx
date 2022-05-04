7d1bbbc3bea21387d3dca07754a4a70367c97093
Alex Fourmy <alexfourmy@gmail.com>
2022-05-04 18:23:48 +0100

    Added a new professional experience

--- src/components/skills/Experience.jsx
+++ src/components/skills/Experience.jsx
@@ -5,21 +5,21 @@ export default function Experience() {
     <div className="experience">
       <h3>Experience</h3>
       <div className="exp1">
+        <h4>Junior Frontend Developer - PageSuite</h4>
+        <h5>March 2022 to present</h5>
+        <p>
+          Assist the development team with all aspects of software design and
+          coding, work on bug fixes, monitor the technical performance of
+          internal systems.
+        </p>
+      </div>
+      <div className="exp2">
         <h4>VAT Registration Representative - Sovos Accordance</h4>
         <h5>November 2019 - April 2021</h5>
         <p>
           Support clients throughout the VAT registration process, collating all
           information required and liaising with European Tax Authorities and
           internal departments.
-        </p>
-      </div>
-      <div className="exp2">
-        <h4>Customer Relationship Manager - Fender</h4>
-        <h5>January 2018 - Ocotber 2019</h5>
-        <p>
-          Assist customers, District Sales Managers and retailers with enquiries
-          related to customer service, orders, stock, product information and
-          returns. Awarded “Employee of the Year” 2018.
         </p>
       </div>
     </div>
