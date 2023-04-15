f4f9be91c88df348009977fd16d566cce99fffb1
Alex Fourmy <alexfourmy@gmail.com>
2023-04-15 12:42:42 +0100

    Updated portfolio with latest projects

--- src/components/skills/Experience.jsx
+++ src/components/skills/Experience.jsx
@@ -8,12 +8,29 @@ export default function Experience() {
         <h4>Junior Frontend Developer - PageSuite</h4>
         <h5>March 2022 to present</h5>
         <p>
-          Assist the development team with all aspects of software design and
-          coding, work on bug fixes, monitor the technical performance of
-          internal systems.
+          For a leading publishing company offering a digital desktop reader and
+          a PWA to 1200+ publishers worldwide, such as The Times, The Sun, and
+          Rolling Stones.
+        </p>
+        <p>
+          • Learnt and worked daily with Angular and Sencha Touch frameworks,
+          C#, TypeScript, and AWS.
+        </p>
+        <p>• Worked on bug fixes, code reviews, and new feature creations.</p>
+        <p>
+          • Maintained graphic standards, code best practices, and branding
+          throughout the product’s interface.
+        </p>
+        <p>
+          • Trusted to lead daily team stand-ups and other feature presentation
+          meetings.
+        </p>
+        <p>
+          • Created new documentation for every new feature or bug fixes to
+          include in the company’s product knowledge base.
         </p>
       </div>
-      <div className="exp2">
+      {/* <div className="exp2">
         <h4>VAT Registration Representative - Sovos Accordance</h4>
         <h5>November 2019 - April 2021</h5>
         <p>
@@ -21,7 +38,7 @@ export default function Experience() {
           information required and liaising with European Tax Authorities and
           internal departments.
         </p>
-      </div>
+      </div> */}
     </div>
   )
 }
